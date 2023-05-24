fe24732ab56f75e8beffd8faccc45ba3ec5efc70
Hunter Haugen <hunter@haugens.org>
2023-05-24 12:31:12 -0700

    Updates so spec tests pass

--- manifests/instance/db.pp
+++ manifests/instance/db.pp
@@ -15,7 +15,7 @@ define wordpress::instance::db (
   }
   if $create_db_user {
     mysql_user { "${db_user}@${db_host}":
-      password_hash => mysql_password($db_password),
+      password_hash => mysql::password($db_password),
     }
     mysql_grant { "${db_user}@${db_host}/${db_name}.*":
       table      => "${db_name}.*",
